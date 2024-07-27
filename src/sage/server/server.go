09e2443ce8fc574d55a37dff16df1609827572d4
Stephanie You <stephanie@dolthub.com>
2024-07-27 10:42:55 -0700

    add cors support

--- src/sage/server/server.go
+++ src/sage/server/server.go
@@ -12,6 +12,7 @@ var db *sql.DB
 func RunServer() error {
 	db, _ = cmd.ConnectDB(cmd.SAGE_DB_NAME)
 	r := gin.Default()
+	r.Use(corsMiddleware())
 
 	r.POST("/add", addHandler)
 	r.GET("/log", logHandler)
@@ -21,4 +22,19 @@ func RunServer() error {
 	r.Run(":8080")
 
 	return nil
+}
+
+func corsMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
 }
